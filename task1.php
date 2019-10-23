a027b75fe67c0c85e663af2dfd95019b3c1b5523
Alexey Kurdyukov <necroalone@gmail.com>
2019-10-23 22:21:50 +0700

    4

--- task1.php
+++ task1.php
@@ -3,7 +3,6 @@
 function println($text = "") {
 	echo($text."<br>");
 }
-
 function revert($str) {
 	
 	$res = "";
@@ -16,10 +15,9 @@ function revert($str) {
 	
 	return $res;
 }
-
 function task1($edge) {
 	println();
-	println("task1:");
+	println("task1($edge):");
     
     println("1");
     
@@ -32,17 +30,15 @@ function task1($edge) {
         
     } while ($result < $edge);
 }
-
 function task2($str) {
 	println();
-	println("task2:");
+	println("task2($str):");
 	
 	println(revert($str));
 }
-
 function task3($arr) {
 	println();
-	println("task3:");
+	println("task3([".implode(",",$arr)."]):");
 	
 	if (gettype($arr[3]) == "string" || gettype($arr[6]) == "string" || gettype($arr[8]) == "string") {
 		println("Achtung! Achtung!");
@@ -51,9 +47,21 @@ function task3($arr) {
 	}
 	
 }
-
+function task4($datestart, $dateend, $dayofweek) {
+	println();
+	println("task4($datestart, $dateend, $dayofweek):");
+	
+	$date = $datestart;
+		
+	while($date <= $dateend) {
+		if (date('w', strtotime($date)) == $dayofweek)
+			println($date);
+		    $date = strtotime("+1 days", strtotime($date));
+	}
+}
 //main:
 task1(10000);
 task2("abcd");
 task3([1,2,3,4,5,6,7,8,9,10]);
 task3([1,2,3,"жопа",5,6,7,8,9,10]);
+task4("2019-08-1", "2019-08-31", 0);
